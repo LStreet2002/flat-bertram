03b014e3adef997ead5e4a130e767d9b75fdf235
Lewis Street <lstreet@salisbury6c.ac.uk>
2019-10-08 14:52:06 +0100

    it is funny now

--- betdurt.js
+++ betdurt.js
@@ -8,11 +8,17 @@ function initialQuestion(){
     var text;
     switch(Input){
       case Input='comedy':
-        text = "You have reached Comedy, you can ask me for a joke";
+        text = "You have reached Comedy, Say joke for a joke";
         break;
       case Input='business':
         text = "You have reached Business,You can ask me where Babies come from";
         break;
+        case Input='joke':
+          text = "Why is apple so dark? It has no windows.";
+          break;
+          case Input='where do babies come from':
+            text = "Storks go fwoosh and bring them to people"
+            break;
       default:
         text = "I'm sorry, I don't understand";}
         document.getElementById("Helpdesk").innerHTML=  text;
