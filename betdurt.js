fe4a857f687fff4aeb96d88f3ae9e7115f8defe1
Lewis Street <lstreet@salisbury6c.ac.uk>
2019-10-08 15:29:33 +0100

    longer text box(30 characters)

--- betdurt.js
+++ betdurt.js
@@ -6,21 +6,29 @@ function initialQuestion(){
 
     var Input=document.getElementById("input").value;
     var text;
+    var title;
     switch(Input){
       case Input='comedy':
         text = "You have reached Comedy, Say joke for a joke";
+        title = "Comedy";
         break;
       case Input='business':
         text = "You have reached Business,You can ask me where Babies come from";
+        title = "Business";
         break;
         case Input='joke':
           text = "Why is apple so dark? It has no windows.";
+          title = "The Joke";
           break;
           case Input='where do babies come from':
-            text = "Storks go fwoosh and bring them to people"
+            text = "You take the moon And you take the sun You take everything that sounds like fun. You stir it all together and then you're done. Rada rada rada rada rada rada. So come on in, feel free to do some looking.Stay a while 'cause somethings always cooking.Come on in, feel free to do some looking. Stay a while 'cause somethings always cooking. Yeah!!!";
+            title = "Facts Of Life";
             break;
       default:
-        text = "I'm sorry, I don't understand";}
+        text = "I'm sorry, I don't understand";
+        title = "Comedy Or Business"}
+      
         document.getElementById("Helpdesk").innerHTML=  text;
+        document.getElementById("firstChoice").innerHTML= title
 
 }
