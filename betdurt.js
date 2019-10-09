954af9ad2cc88d680fd2e82215d6acfe35a1baa3
Lewis Street <lstreet@salisbury6c.ac.uk>
2019-10-09 12:57:19 +0100

    Fancy colour changes

--- betdurt.js
+++ betdurt.js
@@ -10,23 +10,28 @@ function initialQuestion(){
     switch(Input){
       case Input='comedy':
         text = "You have reached Comedy, Say joke for a joke";
-        title = "Comedy";
+        title = "Comedy"
+        document.getElementById("span").style.backgroundColor = "lightgreen";
         break;
       case Input='business':
-        text = "You have reached Business,You can ask me where Babies come from";
-        title = "Business";
+        text = "You have reached Business,You can ask me: 'where do babies come from'";
+        title = "Business"
+        document.getElementById("span").style.backgroundColor = "sandybrown";
         break;
         case Input='joke':
           text = "Why is apple so dark? It has no windows.";
-          title = "The Joke";
+          title = "The Joke"
+          document.getElementById("span").style.backgroundColor = "lightgreen";
           break;
           case Input='where do babies come from':
             text = "You take the moon And you take the sun You take everything that sounds like fun. You stir it all together and then you're done. Rada rada rada rada rada rada. So come on in, feel free to do some looking.Stay a while 'cause somethings always cooking.Come on in, feel free to do some looking. Stay a while 'cause somethings always cooking. Yeah!!!";
-            title = "Facts Of Life";
+            title = "Facts Of Life"
+            document.getElementById("span").style.backgroundColor = "sandybrown";
             break;
       default:
-        text = "I'm sorry, I don't understand";
-        title = "Comedy Or Business"}
+        text = "I'm sorry, your vibes are rancid, Please ask for 'Comedy' or 'Business'";
+        title = "Comedy Or Business"
+        document.getElementById("span").style.backgroundColor = "lightcoral";}
       
         document.getElementById("Helpdesk").innerHTML=  text;
         document.getElementById("firstChoice").innerHTML= title
